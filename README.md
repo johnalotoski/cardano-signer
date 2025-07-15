e9dee4e1ad6d3b1d8ab92b5aa2427206b83a44cd
John Lotoski <john.lotoski@iohk.io>
2025-07-15 17:29:53 -0500

    init: make it build!

---
+++ README.md
@@ -0,0 +1,69 @@
+# cardano-signer (Nix Flake)
+
+This project packages the `cardano-signer` Node.js CLI using Nix and Yarn from
+the excellent upstream repo:
+[cardano-signer](https://github.com/gitmachtl/cardano-signer).
+
+---
+
+## Building with Nix
+
+To build the cardano-signer:
+
+    nix build -L .#cardano-signer
+
+The CLI binary will be available to run at:
+
+    ./result/bin/cardano-signer
+
+---
+
+## Running remotely
+
+To run the CLI tool from this flake without cloning:
+
+    nix run github:johnalotoski/cardano-signer
+
+You can also run from a local flake path:
+
+    nix run .#cardano-signer
+
+---
+
+## Development Shell
+
+To enter a dev environment with `node`, `yarn`, and helper tools:
+
+    nix develop
+
+or, if you have [direnv](https://direnv.net) installed:
+
+    direnv allow
+
+---
+
+## Updating the cardano-signer version
+
+* Update the commit found at `srcRepo.rev` in `flake.nix` and save
+* Reload the devShell to preload the new source repo
+* Run `update-yarn-file` to generate a new `yarn-$SHORT_REV.lock`
+* Make sure the build succeeds with:
+
+    nix build -L .#cardano-signer
+
+* Remove the old `yarn-$OLD_SHORT_REV.lock` file
+* Commit the changes
+* Celebrate! :)
+
+---
+
+## Directory layout after build
+
+    result/
+    ├── bin/
+    │   └── cardano-signer
+    └── libexec/cardano-signer/
+        ├── deps/
+        │   └── cardano-signer/
+        ├── node_modules/
+        └── ...
---
+++ flake.nix
@@ -0,0 +1,124 @@
+{
+  description = "A nix flake for https://github.com/gitmachtl/cardano-signer";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    srcRepo = pkgs:
+      pkgs.fetchFromGitHub {
+        owner = "gitmachtl";
+        repo = "cardano-signer";
+        rev = "2f23e2904ae08b98d3e1d383c0e5f2b94af30db8";
+        sha256 = "sha256-eeMonqsvFMqIZ2I/xdx69AkHMecnDjbPK1VmmFvO2hk=";
+      };
+
+    shortRev = commit: builtins.substring 0 7 commit;
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      with pkgs; {
+        packages = rec {
+          default = cardano-signer;
+
+          cardano-signer = let
+            cardano-signer' = mkYarnPackage rec {
+              name = "cardano-signer";
+              version = "${shortRev src.rev}";
+
+              src = srcRepo pkgs;
+
+              nativeBuildInputs = [makeWrapper];
+
+              packageJSON = "${src.outPath}/src/package.json";
+              yarnLock = ./yarn-${shortRev src.rev}.lock;
+
+              yarnFlags = ["--frozen-lockfile" "--production"];
+
+              installPhase = ''
+                # Expected module output path
+                mkdir -p $out/libexec/cardano-signer
+
+                # Reshape the deps dir
+                mv deps/cardano-signer/src/* deps/cardano-signer/
+                rmdir deps/cardano-signer/src
+                rm -rf deps/cardano-signer/node_modules
+                rm deps/cardano-signer/cardano-signer
+
+                # Assemble under the libexec target
+                cp -r deps node_modules $out/libexec/cardano-signer/
+
+                # Create a bin wrapper
+                makeWrapper ${nodejs}/bin/node $out/bin/cardano-signer \
+                --add-flags "$out/libexec/cardano-signer/deps/cardano-signer/cardano-signer.js" \
+                --set NODE_PATH "$out/libexec/cardano-signer/node_modules"
+              '';
+            };
+          in
+            runCommand "cardano-signer-${shortRev (srcRepo pkgs).rev}" {} ''
+              # Clean up unneeded tarball
+              mkdir $out
+              cp -r ${cardano-signer'}/* $out
+              chmod -R +w $out/tarballs
+              rm -rf $out/tarballs
+            '';
+        };
+
+        devShells.default = let
+          update-yarn-lock = writeShellApplication {
+            name = "update-yarn-lock";
+            text = ''
+              PKG_JSON="${(srcRepo pkgs).outPath}/src/package.json"
+              cp "$PKG_JSON" .
+              chmod +w package.json
+              yarn install
+              cp -f yarn.lock yarn-${shortRev (srcRepo pkgs).rev}.lock
+              rm -rf yarn.lock package.json node_modules
+            '';
+          };
+        in
+          mkShell {
+            packages = [
+              coreutils
+              figlet
+              gnutar
+              gzip
+              jq
+              lolcat
+              nodejs
+              update-yarn-lock
+              yarn
+            ];
+
+            shellHook = ''
+              figlet <<< "Cardano-signer devShell." | lolcat
+              echo
+              echo "Build:"
+              echo
+              echo "  nix build -L .#cardano-signer"
+              echo
+              echo "Run Locally:"
+              echo
+              echo "  result/bin/cardano-signer \"\$ARGS[@]\""
+              echo
+              echo "Run from repo remotely:"
+              echo
+              echo "  nix run github:johnalotoski/cardano-signer -- \"\$ARGS[@]\""
+              echo
+              echo "Update (see README.md for more details):"
+              echo
+              echo "  update-yarn-lock"
+              echo
+            '';
+          };
+      });
+}
