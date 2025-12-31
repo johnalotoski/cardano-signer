0d52372ca05225293c7b89bfd97c42c1de92b689
perturbing <thomas.vellekoop@iohk.io>
2025-12-31 13:24:47 +0100

    update cardano-signer to 1.34.0

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
       pkgs.fetchFromGitHub {
         owner = "gitmachtl";
         repo = "cardano-signer";
-        rev = "v1.29.0";
-        sha256 = "sha256-y8yUsmSakaR6foKh/KnRTHctZu3nN/gI0h87wt88suU=";
+        rev = "v1.34.0";
+        sha256 = "sha256-9dJpZnEm4adZerDvbtglVHIQIi9P1byAtrKFnh0RBB0=";
       };
 
     shortRevOrRelTag = rev:
